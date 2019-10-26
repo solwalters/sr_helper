c9aa46afc76f51d4450133c6659e6410aadf5851
Sol Walters <sol.walters@gmail.com>
2019-10-26 15:54:05 -0600

    added attack roll form

--- shadowrun.js
+++ shadowrun.js
@@ -10,6 +10,10 @@ function RollDicePool(pool){
     results.sixes = 0;
     results.glitch = '';
 
+    if (pool == 0){
+        return results;
+    }
+
     var singleRoll;
     for (var i = 0; i < pool; i++) {
         singleRoll = RollD6();
@@ -34,8 +38,6 @@ function RollDicePool(pool){
     return results;
 }
 
-
-
 function RollSingleDicePoolBtn(){
     document.getElementById("rollSingleDicePoolResults").innerHTML = '';
     var value = document.getElementById("rollSingleDicePoolValue").value;
@@ -44,12 +46,85 @@ function RollSingleDicePoolBtn(){
     }
     else {
         var dicePool = RollDicePool(value);
-        console.log(dicePool);
         var results = "DicePool: " + value + "<br/>Hits: " + dicePool.hits + "<br/>Misses: " + dicePool.misses;
         if (dicePool.glitch != ''){
             results += "<br/><b>" + dicePool.glitch + "</b>";
         }
         document.getElementById("rollSingleDicePoolResults").innerHTML = results;
+    }
+}
+
+function RollCombatBtn(){
+    document.getElementById("rollCombatResults").innerHTML = '';
+
+    var at_dp = document.getElementById("attackerDicePoolValue").value;
+    var at_dm = document.getElementById("attackerDamageValue").value;
+    var de_dp = document.getElementById("defenderDicePoolValue").value;
+    var de_so = document.getElementById("defenderSoakValue").value;
+
+    if (at_dp == '') {
+        at_dp = 0;
+    }
+    if (at_dm == '') {
+        at_dm = 0;
+    }
+    if (de_dp == '') {
+        de_dp = 0;
+    }
+    if (de_so == '') {
+        de_so = 0;
+    }
+
+    if (isNaN(at_dp) || isNaN(at_dm) || isNaN(de_dp) || isNaN(de_so)) {
+        document.getElementById("rollCombatResults").innerHTML = 'Missing a numeric value.';
+    }
+    else {
+        var p_at_dp = RollDicePool(at_dp);
+        var p_de_dp = RollDicePool(de_dp);
+        var p_de_so = RollDicePool(de_so);
+
+        var results = '';
+
+        results +=
+            "AttackerDicePool: " + at_dp +
+            "<br/>Hits: " + p_at_dp.hits +
+            "<br/>Misses: " + p_at_dp.misses;
+        if (p_at_dp.glitch != ''){
+            results += "<br/><b>Attacker: " + p_at_dp.glitch + "</b>";
+        }
+        results +=
+            "<br/>DefenderDicePool: " + de_dp +
+            "<br/>Hits: " + p_de_dp.hits +
+            "<br/>Misses: " + p_de_dp.misses;
+        if (p_de_dp.glitch != ''){
+            results += "<br/><b>Defender: " + p_de_dp.glitch + "</b>";
+        }
+
+        if (p_de_dp.hits >= p_at_dp.hits) {
+            results += "<br/><br/><b>Miss.</b>";
+        }
+        else {
+            var netHits = p_at_dp.hits - p_de_dp.hits
+            var netDmg = Number(at_dm) + Number(netHits);
+            results += "<br/><br/><b>Hit. " + netHits +
+                " net hits</b> added to damage.<br/>Rolling soak vs " + netDmg;
+            results +=
+                "<br/>DefenderSoakPool: " + de_so +
+                "<br/>Hits: " + p_de_so.hits +
+                "<br/>Misses: " + p_de_so.misses;
+            if (p_de_so.glitch != '') {
+                results += "<br/><b>Defender: " + p_de_so.glitch + "</b>";
+            }
+            if (p_de_so.hits > netDmg) {
+                results += "<br/>No damage.";
+            }
+            else {
+                var dmg = Number(netDmg) - Number(p_de_so.hits);
+                results += "<br>" + dmg + " damage.";
+            }
+        }
+
+        document.getElementById("rollCombatResults").innerHTML = results;
     }
 }
 
